c4279897f975e9c55e56879058de212e12c7c703
SamA <sam.atkin@dcms.gov.uk>
2020-12-18 14:30:23 +0000

    Updated doccumentation of is_integer64, updated NAMESPACE, updated DESCRIPTION

--- R/is_integer64.R
+++ R/is_integer64.R
@@ -1,3 +1,11 @@
+#' Return true or false if the object is integer64 type
+#' @param x is an object
+#'
+#' @return boolean
+#' @export
+#' @examples
+#' is_integer64(100)
+
 is_integer64 <- function(x){
   class(x)=="integer64"
 }
