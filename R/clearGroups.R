977a7b7505fe73313a3b3e228c5002b6831cb349
SamA <sam.atkin@dcms.gov.uk>
2021-01-04 13:43:24 +0000

    d

--- R/clearGroups.R
+++ R/clearGroups.R
@@ -1,6 +1,7 @@
 #' Function to clear all groups from a leaflet object from within a shiny application.
 #'
 #' @param map a string containing the name of the map
+#' @param ... a comma seperated set of strings that contain the group names to clear
 
 
 clearGroups<-function(map,...){
